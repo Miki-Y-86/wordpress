15fc1ed0464472770d4b1a0d7e302f0d67e58641
Miki-Y-86 <53904005+Miki-Y-86@users.noreply.github.com>
2022-09-17 08:26:27 +0900

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,62 @@
+<div id="sidebar" class="sidebar-widget">
+    <div class="sidebar-item">
+        <h2>新着記事</h2>
+        <ul>
+            <li>
+                <a href="">Hello world!</a>
+            </li>
+            <li>
+                <a href="">Hello world!</a>
+            </li>
+            <li>
+                <a href="">ブログサンプルタイトル</a>
+            </li>
+            <li>
+                <a href="">サンプルブログ投稿</a>
+            </li>
+            <li>
+                <a href="">Hello world!</a>
+            </li>
+        </ul>
+    </div>
+    <div class="sidebar-item">
+        <h2>カテゴリー</h2>
+        <ul>
+            <li>
+                <a href="">ブログ(10)</a>
+            </li>
+            <li>
+                <a href="">未分類(2)</a>
+            </li>
+            <li>
+                <a href="">ニュース(27)</a>
+            </li>
+            <li>
+                <a href="">イベント(8)</a>
+            </li>
+            <li>
+                <a href="">その他(3)</a>
+            </li>
+        </ul>
+    </div>
+    <div class="sidebar-item">
+        <h2>アーカイブ</h2>
+        <ul>
+            <li>
+                <a href="">2022.09.01</a>
+            </li>
+            <li>
+                <a href="">2022.08.28</a>
+            </li>
+            <li>
+                <a href="">2022.05.22</a>
+            </li>
+            <li>
+                <a href="">2022.03.18</a>
+            </li>
+            <li>
+                <a href="">2022.01.09</a>
+            </li>
+        </ul>
+    </div>
+</div>
